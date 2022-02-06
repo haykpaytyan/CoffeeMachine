5a68a5495dd4322475de92f94d1eac607b9d804d
Hayk Paytyan <86311205+haykpaytyan@users.noreply.github.com>
2022-02-06 17:04:33 +0400

    Coffee Machine

--- src/com/aca/Main.java
+++ src/com/aca/Main.java
@@ -1,21 +1,22 @@
 package com.aca;
 
 import com.aca.actionXndir4.Action;
+import com.aca.enoughCoffeeXndir3.EnoughCoffee;
 import com.aca.oncoffeeloopXndir5.OnCoffeeLoop;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // PrintWhatIsNeeded.print();
         // CalculateAmountOfIngredients.calculate(25);
 
-//        EnoughCoffee enoughCoffee=new EnoughCoffee();
-//        enoughCoffee.ifWeHaveEnoughSupplies(25);
+//        EnoughCoffee enoughCoffee = new EnoughCoffee();
+//        enoughCoffee.ifWeHaveEnoughSupplies();
 //
 //        Action action = new Action();
 //        action.choose();
 
-        OnCoffeeLoop coffee = new OnCoffeeLoop();
-        coffee.choose();
 
     }
 }
--- src/com/aca/enoughCoffeeXndir3/EnoughCoffee.java
+++ src/com/aca/enoughCoffeeXndir3/EnoughCoffee.java
@@ -9,30 +9,30 @@ public class EnoughCoffee {
     private final int coffee = 15;
 
 
-    public void ifWeHaveEnoughSupplies(int countOfCoffee) {
+    public void ifWeHaveEnoughSupplies() {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("amount of water");
+        System.out.println("Write how many ml of water the coffee machine has:");
         int wwater = scanner.nextInt();
-        System.out.println("amount of milk");
+        System.out.println("Write how many ml of milk the coffee machine has:");
         int mmilk = scanner.nextInt();
-        System.out.println("amount of coffee");
-
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
         int ccoffee = scanner.nextInt();
-
+        System.out.println("Wrtie how many cups of coffee you will need");
+        int countOfCoffee = scanner.nextInt();
         if (wwater >= water * countOfCoffee && mmilk >= milk * countOfCoffee && ccoffee >= coffee * countOfCoffee) {
 
             if (wwater < water * (countOfCoffee + 1) || mmilk < milk * (countOfCoffee + 1) || ccoffee < coffee * (countOfCoffee + 1)) {
                 System.out.println("Yes, I can make that amount of coffee\n");
             } else {
 
-                int remainWater = (wwater - water * countOfCoffee) / countOfCoffee;
-                int remainMilk = (wwater - milk * countOfCoffee) / countOfCoffee;
-                int remainCoffee = (wwater - coffee * countOfCoffee) / countOfCoffee;
+                int remainWater = (wwater - water * countOfCoffee) / water;
+                int remainMilk = (mmilk - milk * countOfCoffee) / milk;
+                int remainCoffee = (ccoffee - coffee * countOfCoffee) / coffee;
 
                 int n = Math.min(remainCoffee, Math.min(remainMilk, remainWater));
 
-                System.out.println("Yes, I can make that amount of coffee end even more" + n + "more coffee\n");
+                System.out.println("Yes, I can make that amount of coffee (and even " + n + " more than that)\n");
 
             }
 
